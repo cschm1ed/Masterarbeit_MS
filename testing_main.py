ea69ef893f06524da32babc22801be222f69701e
maxischbrt <gantermaxi@gmail.com>
2023-09-26 20:10:48 +0200

    Automatisiertes Fahren von Referenzfahrten

--- testing_main.py
+++ testing_main.py
@@ -1,10 +1,25 @@
 # Import der ganzen Funktionen:
+import time
 
-from functions import open_Estlcam
-from functions import startReferenceRun
-from functions import startDatalogging_57600
+import functions
 
-# Aufrufen der Funktion openEslcam
-open_Estlcam()
-startReferenceRun()
-startDatalogging_57600("Funktionstest",10)
+
+# testing_main
+# Aufrufen der Funktion:
+# Aktuell mit Abtastrate von 50 Hz; aufgrund des USS
+
+
+prozess = functions.open_Estlcam()
+functions.openReferenzrun()
+
+print("--- Start Referenzfahrt: ---")
+for i in range(1,35):
+    name = "Referenzfahrt_" + str(i)
+    functions.startDatalogging_115200(name=name,erfassungsdauer=60)
+    print("     ...." + name + " done")
+    if i % 5 == 0:
+        functions.sentMail('maxi11696@googlemail.com',iteration=i)
+    time.sleep(10)
+
+# todo: Estlcam komplett schließen ???
+prozess.terminate()
